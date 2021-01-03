8561cac652d8edf19456916d8d4e425897aa7c0d
Job Strutt <61621039+bojnin-basile@users.noreply.github.com>
2021-01-03 20:05:04 +0100

    Add files via upload

---
+++ Chords_in_text.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chords in text
+
+# In[106]:
+
+
+inputText = """
+    
+    [III]
+    [II]
+    [IV]
+    [V]
+    [iii]
+    [ii]
+    [VII::chord]
+    [iii][vii][iv][ii][i]
+"""
+print(inputText)
+
+
+
+
+substitutions = [
+    ('[III','[Ⅲ'),
+    ('[VII','[Ⅶ'),
+    ("[II","[Ⅱ"),
+    ('[IV','[Ⅳ'),
+    ('[VI','[Ⅵ'),
+    ("[I", "[Ⅰ"),
+    ('[V','[Ⅴ'),
+    ('iii','ⅲ'),
+    ('[vii','[ⅶ'),
+    ('[ii','[ⅱ'),
+    ('[vi','[ⅵ'),
+    ('[i','[ⅰ'),
+    ('[v','[ⅴ')
+     ]
+
+for search, replacement in substitutions:
+    outputText=inputText.replace(search, replacement);
+    #print('output' + outputText);
+    #print('input'+inputText);
+    inputText = outputText;
+print(inputText)
+
+replace('iv','ⅳ')                       .replace('v','ⅴ').replace('vi','ⅵ').replace('vii','ⅶ');
+outputText
+
+
+
+
+
